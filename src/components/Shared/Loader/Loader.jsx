1115f7080f39cbb4573453545b5cf71ba3187fb8
BeBo <bodiatta5@gmail.com>
2025-05-20 20:01:59 +0300

    first commit

--- src/components/Shared/Loader/Loader.jsx
+++ src/components/Shared/Loader/Loader.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Loading from './loading'
+import Loading from './Loading'
 
 // import loader from '../../../assets/images/Blocks@1x-1.0s-200px-200px.gif'
 export default function Loader() {
